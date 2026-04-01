f6fa9345b87e08a20224abb3b81e67a20942b004
Tat Uyen Tam <tattam192@gmail.com>
2026-04-01 20:40:54 +0700

    today's weather

---
+++ exercise1.ts
@@ -0,0 +1,12 @@
+let temperature = 55;
+let conditions = "sunny";
+let sunriseTime = "7:34 AM";
+let sunsetTime = "5:09 PM";
+
+console.log(temperature);
+console.log(conditions);
+console.log(sunriseTime);
+console.log(sunsetTime);
+
+let concatStr = `It's currently ${conditions} and ${temperature} degrees. The sun rose this morning at ${sunriseTime} and will set at ${sunsetTime}.`;
+console.log(concatStr);
